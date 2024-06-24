70c99a1aefdc73c8e80e0952724839c43ae450bb
Sebasceb <sceballo@espol.edu.ec>
2024-06-24 18:42:23 -0500

    Cambios

--- main.py
+++ main.py
@@ -1,99 +1,168 @@
 import ply.yacc as yacc
 from AnalizadorLexico import tokens
 
-# Rafael Merchan ini
+# Reglas de la gramática
+
 def p_programa(p):
     '''programa : declaracion_lista'''
-
+    pass
 
 def p_declaracion_lista(p):
     '''declaracion_lista : declaracion_lista declaracion
-                        | declaracion'''
-
+                         | declaracion'''
+    pass
 
 def p_declaracion(p):
     '''declaracion : declaracion_var
-                   | declaracion_funcion'''
-
+                   | declaracion_funcion
+                   | declaracion_EC'''
+    pass
 
 def p_declaracion_var(p):
     '''declaracion_var : especificador ID SEMICOLON
-                       | especificador ID EQUAL expresion SEMICOLON'''
-
+                       | especificador ID EQUAL expresion SEMICOLON
+                       | especificador ID EQUAL estructura_dato SEMICOLON'''
+    pass
 
 def p_especificador(p):
     '''especificador : INT
-                      | FLOAT
-                      | DOUBLE
-                      | CHAR
-                      | BOOL'''
-
+                     | FLOAT
+                     | DOUBLE
+                     | CHAR
+                     | STRING
+                     | BOOL'''
+    pass
 
 def p_declaracion_funcion(p):
     '''declaracion_funcion : especificador ID LPAREN parametro RPAREN compound_stmt
-                       | VOID ID LPAREN parametro RPAREN compound_stmt'''
-
+                           | VOID ID LPAREN parametro RPAREN compound_stmt'''
+    pass
 
 def p_parametro(p):
     '''parametro : parametro_lista
-              | VOID'''
-
+                 | VOID'''
+    pass
 
 def p_parametro_lista(p):
-    '''parametro_lista : parametro_lista COMMA parametro
-                  | parametro'''
-
+    '''parametro_lista : parametro_lista COMMA param
+                       | param'''
+    pass
 
 def p_param(p):
-    '''param : type_specifier ID
-             | type_specifier ID LBRACKET RBRACKET'''
+    '''param : especificador ID
+             | especificador ID LBRACKET RBRACKET'''
+    pass
 
 def p_expresion(p):
-    '''expresion : variable EQUAL expresion
-                  | expresion_simple'''
-
-def p_variable(p):
-    '''variable : ID
-           | ID LBRACKET expresion RBRACKET'''
+    '''expresion : ID
+                 | NUMBER
+                 | ID EQUAL expresion
+                 | expresion_simple
+                 | LPAREN expresion RPAREN'''
+    pass
 
 def p_expresion_simple(p):
     '''expresion_simple : expresion_aditiva igualdad expresion_aditiva
-                         | expresion_aditiva'''
+                        | expresion_aditiva'''
+    pass
 
 def p_igualdad(p):
     '''igualdad : LESS
-             | LESSEQUAL
-             | GREATER
-             | GREATEREQUAL
-             | DEQUAL
-             | DISTINT'''
+                | LESSEQUAL
+                | GREATER
+                | GREATEREQUAL
+                | DEQUAL
+                | DISTINT'''
+    pass
 
 def p_expresion_aditiva(p):
     '''expresion_aditiva : expresion_aditiva sumorest termino
-                           | termino'''
-
+                         | termino'''
+    pass
 
 def p_sumorest(p):
     '''sumorest : PLUS
-             | MINUS'''
-
+                | MINUS'''
+    pass
 
 def p_termino(p):
     '''termino : termino multodiv factor
-            | factor'''
+               | factor'''
+    pass
 
 def p_multodiv(p):
     '''multodiv : TIMES
-             | DIVIDE'''
+                | DIVIDE'''
+    pass
 
 def p_factor(p):
     '''factor : LPAREN expresion RPAREN
-              | variable
+              | declaracion_var
               | NUMBER'''
+    pass
 
+def p_estructura_dato(p):
+    '''estructura_dato : array_init
+                       | tuple_init
+                       | vector_init'''
+    pass
 
-# Rafael Merchan fin
+def p_array_init(p):
+    '''array_init : LBRACKET expresion_list RBRACKET'''
+    pass
 
+def p_tuple_init(p):
+    '''tuple_init : LPAREN expresion_list RPAREN'''
+    pass
+
+def p_vector_init(p):
+    '''vector_init : LGREATER expresion_list RGREATER'''
+    pass
+
+def p_expresion_list(p):
+    '''expresion_list : expresion_list COMMA expresion
+                      | expresion
+                      | empty'''
+    pass
+
+# Estructuras de control (if/if-else, switch, while)
+def p_declaracion_EC(p):
+    '''declaracion_EC : selection_stmt
+                      | iteration_stmt'''
+    pass
+
+def p_selection_stmt(p):
+    '''selection_stmt : IF LPAREN expresion RPAREN statement
+                      | IF LPAREN expresion RPAREN statement ELSE statement
+                      | SWITCH LPAREN expresion RPAREN statement'''
+    pass
+
+def p_iteration_stmt(p):
+    '''iteration_stmt : WHILE LPAREN expresion RPAREN statement'''
+    pass
+
+def p_statement(p):
+    '''statement : expresion_stmt
+                 | compound_stmt'''
+    pass
+
+def p_expresion_stmt(p):
+    '''expresion_stmt : expresion SEMICOLON
+                      | SEMICOLON'''
+    pass
+
+def p_compound_stmt(p):
+    '''compound_stmt : LBLOCK statement_list RBLOCK'''
+    pass
+
+def p_statement_list(p):
+    '''statement_list : statement_list statement
+                      | statement'''
+    pass
+
+def p_empty(p):
+    '''empty :'''
+    pass
 
 # Error rule for syntax errors
 def p_error(p):
@@ -103,10 +172,10 @@ def p_error(p):
 parser = yacc.yacc()
 
 while True:
-   try:
-       s = input('lp > ')
-   except EOFError:
-       break
-   if not s: continue
-   result = parser.parse(s)
-   print(result)
+    try:
+        s = input('lp > ')
+    except EOFError:
+        break
+    if not s: continue
+    result = parser.parse(s)
+    print(result)
